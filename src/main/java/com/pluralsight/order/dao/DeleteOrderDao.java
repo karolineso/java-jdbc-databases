8d62574e2d76b16f8374cd93f3b4fe33a43fa1d2
karolineso <83075114+karolineso@users.noreply.github.com>
2021-05-03 11:21:23 -0300

    Update DeleteOrderDao.java

--- src/main/java/com/pluralsight/order/dao/DeleteOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/DeleteOrderDao.java
@@ -1,69 +1,131 @@
 package com.pluralsight.order.dao;
 
 import com.pluralsight.order.dto.ParamsDto;
-import com.pluralsight.order.util.Database;
+import com.pluralsight.order.util.Datebase;
 import com.pluralsight.order.util.ExceptionHandler;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
+import java.sql.util.List;
 
 /**
- * DAO to delete an order
+ * DAO to delete n order
  */
-public class DeleteOrderDao {
+public class DeleterOrderDao {
     private Database database;
-
+    
     /**
-     * Constructor
-     * @param database Database object
-     */
+    * Constructor
+    * @param database Database object
+    */
     public DeleteOrderDao(Database database) {
         this.database = database;
     }
-
+ 
     /**
-     * Deletes one or more orders using their IDs
-     * @param paramsDto Object with the parameters for the operation
+     * Delets one or more orders using their IDs
+     * @param paramsDto Object with the paramenters for the operation
      * @return Number of orders deleted
      */
-    public int deleteOrdersById(ParamsDto paramsDto) {
-        int numberResults = 0;
-
+    public int deleteOrdersById(ParamsDto paramDto) {
+        int numbersResults = 0;
+        
         try (Connection con = null;
              PreparedStatement ps = createPreparedStatement(con, paramsDto.getOrderIds())
         ) {
-
+            
         } catch (SQLException ex) {
-            ExceptionHandler.handleException(ex);
+            ExceptionHandler.handlerException(ex);
         }
-
+        
         return numberResults;
     }
-
+    
     /**
      * Method to build the delete SQL statement
-     * @param orderIds IDs of the orders to delete
-     * @return Delete SQL statement
-     */
+     * @param orderIds IDs of the orders to delete 
+     * @return Delete SQL statement 
+     */ 
     private String buildDeleteSql(List<Long> orderIds) {
         String ids = null;
-
+        
         return "DELETE FROM orders o WHERE o.order_id IN (" + ids + ")";
     }
 
     /**
      * Creates a PreparedStatement object to delete one or more orders
-     * @param con Connnection object
+     * @param con Connection object 
      * @param orderIds Order IDs to set on the PreparedStatement
-     * @return A PreparedStatement object
-     * @throws SQLException In case of an error
+     * @return A PreparedStatement object 
+     * @throws SQLException In case of an error 
      */
     private PreparedStatement createPreparedStatement(Connection con, List<Long> orderIds) throws SQLException {
-        String sql = buildDeleteSql(orderIds);
+        String sql = buildDeleteSql(ordersIds);
         PreparedStatement ps = null;
-
+        
         return ps;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+ 
+
+
+
+
+ 
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+
+  
+    
+     
+    
+    
+   
+       
+        
+
+        
+    
+
